aba8513c9ab22531b4ef4ab410d493f023694ecc
Oulinstar <terleeva89@gmail.com>
2023-09-28 16:44:33 +0200

    Finish game

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,41 @@
+const gameData = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+]
+
+
+let editedPlayer = 0;
+let activePlayer = 0;
+let currentRound = 1;
+let gameIsOver = false;
+
+const players = [
+    {
+        name: '',
+        symbol: 'X'
+    },
+    {
+        name: '',
+        symbol: 'O'
+    },
+];
+
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-errors');
+const gameAreaElement = document.getElementById('active-game');
+const activePlayerName = document.getElementById('active-player-name');
+const gameOverElement = document.getElementById('game-over');
+
 
 const editPlayer1BtnElement = document.getElementById('edit-player1');
 const editPlayer2BtnElement = document.getElementById('edit-player2');
 const cancelConfigBtnElement = document.getElementById('cancel-btn');
+const startNewGameBtn = document.getElementById('start-game-btn');
+const gameFieldElements = document.querySelectorAll('#game-board li');
+const gameFieldElement = document.getElementById('game-board');
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
@@ -14,3 +44,9 @@ cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
 
 formElement.addEventListener('submit', savePlayerConfig);
+
+startNewGameBtn.addEventListener('click', startNewGame);
+
+for (const gameFieldElement of gameFieldElements) {
+    gameAreaElement.addEventListener('click', selectGamenField);
+}
--- scripts/config.js
+++ scripts/config.js
@@ -1,14 +1,16 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+    editedPlayer = +event.target.dataset.playerid;   //+ before a string converts it into a number +'1' => 1
     playerConfigOverlayElement.style.display = 'block';
     backdropElement.style.display = 'block';
 }
 
 function closePlayerConfig() {
+
     playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
     formElement.firstElementChild.classList.remove('error');
     errorsOutputElement.textContent = '';
-
+    formElement.firstElementChild.lastElementChild.value = '';   //could be better to do with id, but just practice DOM structure
 
 }
 
@@ -22,4 +24,10 @@ function savePlayerConfig(event) {
         errorsOutputElement.textContent = 'Please enter a valid name';
         return;
     }
+
+    const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
+    updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+    players[editedPlayer - 1].name = enteredPlayerName;
+
+    closePlayerConfig();
 }
